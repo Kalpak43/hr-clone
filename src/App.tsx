2bb620b3d771df632b7da055a465f35cbe7d8c21
Kalpak43 <111725804+Kalpak43@users.noreply.github.com>
2025-03-18 15:08:09 +0530

    added react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
+import { Routes } from "react-router";
 import "./App.css";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import Sidebar from "./components/Sidebar";
+import Layout from "./Layout";
+import { Route } from "react-router";
+import HomePage from "./pages/HomePage";
+import EmployeesPage from "./pages/EmployeesPage";
 
 function App() {
   return (
-    <main className="flex divide-x divide-gray-300 h-screen">
-      <Sidebar />
-      <div className="w-full md:w-3/4 lg:w-4/5 xl:w-5/6 h-full flex flex-col">
-        <Header />
-        <Hero />
-      </div>
-    </main>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/employees" element={<EmployeesPage />} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ src/Layout.tsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import { Outlet } from "react-router";
+
+function Layout() {
+  return (
+    <main className="flex divide-x divide-gray-300 h-screen">
+      <Sidebar />
+      <div className="w-full md:w-3/4 lg:w-4/5 xl:w-5/6 h-full flex flex-col">
+        <Header />
+        <Outlet />
+      </div>
+    </main>
+  );
+}
+
+export default Layout;
